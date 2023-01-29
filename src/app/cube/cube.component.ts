bc30cfed76b2897fa15989b25fb5f80a0c8bbb98
BastienTLC <bastientalec@orange.fr>
2023-01-29 15:27:45 +0100

    Modification three 3D

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -1,7 +1,11 @@
-import {Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input, HostListener} from '@angular/core';
 import * as THREE from 'three';
-import {CanvasTexture} from "three";
+import {CanvasTexture, Object3D} from "three";
 import {FlakesTexture} from "three/examples/jsm/textures/FlakesTexture";
+import {GLTF, GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {animate} from "@angular/animations";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 
 @Component({
@@ -11,39 +15,50 @@ import {FlakesTexture} from "three/examples/jsm/textures/FlakesTexture";
 })
 export class CubeComponent implements OnInit, AfterViewInit{
   @ViewChild('canvas')
+
+
   private canvasRef: ElementRef;
-  @Input() public rotationSpeedX: number = 0.05;
-  @Input() public rotationSpeedY: number = 0.1;
+  @Input() public rotationSpeedX: number = 0.005;
+  @Input() public rotationSpeedY: number = 0.005;
   @Input() public size: number = 200;
-  @Input() public texture: string = "../assets/textures/texture.jpg";
+  @Input() public texture: string = "../../assets/textures/texture.jpg";
 
-  //Satge properties
-
-  @Input() public cameraZ: number = 400;
+  @Input() public cameraZ: number = 200;
   @Input() public fieldOfView: number = 1;
   @Input('nearClipping') public nearClippingPlane:number = 1;
   @Input('farClipping') public farClippingPlane: number = 1000;
-
-
-
-
 
 
   private camera!: THREE.PerspectiveCamera;
   private get canvas(): HTMLCanvasElement { return this.canvasRef.nativeElement;}
   private loader = new THREE.TextureLoader();
   private geometry = new THREE.BoxGeometry(1, 1, 1);
+  private light = new THREE.DirectionalLight(0xffffff,1);
+  private import = new GLTFLoader();
+  private obj:Object3D = new THREE.Object3D;
+
+
   private material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture)});
   private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
-
+  //private controls = new OrbitControls(this.camera, this.renderer.domElement);
 
   private createScene() {
 //* Scene
+    this.light.position.set(2,2,5);
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x000000)
-    this.scene.add(this.cube);
+    //this.scene.background = new THREE.Color(0x0000FF);
+    //this.scene.add(this.cube);
+    this.import.load('../../assets/3D/character_rogue.gltf', (gltf) =>{
+       this.obj = gltf.scene.children[0];
+      this.scene.add(this.obj);
+    }, undefined, function (error){
+      console.error(error);
+    });
+
+    this.scene.add(this.light);
+
 //*Camera
     let aspectRatio = this.getAspectRatio();
     this.camera = new THREE. PerspectiveCamera(
@@ -52,30 +67,35 @@ export class CubeComponent implements OnInit, AfterViewInit{
         this.nearClippingPlane,
         this.farClippingPlane
     )
-    this.camera.position.z = this.cameraZ;
+   this.camera.position.z = this.cameraZ;
   }
+
+
   private getAspectRatio() {
     return this.canvas.clientWidth / this.canvas.clientHeight;
   }
 
   private  animateCube(){
-    this.cube.rotation.x += this.rotationSpeedX;
-    this.cube.rotation.y += this.rotationSpeedY;
+    this.obj.rotation.y += this.rotationSpeedY;
+    this.obj.rotation.x += this.rotationSpeedX;
   }
 
   private startRenderingLoop() {
 //* Renderer
 // Use canvas element in template
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, alpha: true });
     this.renderer.setPixelRatio (devicePixelRatio);
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
     let component: CubeComponent = this;
+    const control = new OrbitControls(this.camera, this.renderer.domElement);
+    control.update();
     (function render() {
       requestAnimationFrame(render);
-      component.animateCube();
+      //component.animateCube();
       component.renderer.render(component.scene, component.camera);
     }());
   }
+
 
   ngOnInit() {
   }
@@ -84,5 +104,10 @@ export class CubeComponent implements OnInit, AfterViewInit{
 
     this.createScene();
     this.startRenderingLoop();
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(event : WindowEventMap) {
+    console.log("scroll");
   }
 }
--- src/app/list-ville/line-chart/line-chart.component.ts
+++ src/app/list-ville/line-chart/line-chart.component.ts
@@ -3,7 +3,6 @@ import {Chart, ChartConfiguration, ChartEvent, ChartType, elements} from 'chart.
 import { BaseChartDirective } from 'ng2-charts';
 import {VilleModel} from "../ville.model";
 import _default from "chart.js/dist/core/core.interaction";
-import index = _default.modes.index;
 
 
 @Component({
@@ -59,7 +58,6 @@ export class LineChartComponent {
             }
         },
         scales: {
-            // We use this empty structure as a placeholder for dynamic theming.
             y:
                 {
                     position: 'left',
@@ -85,51 +83,4 @@ export class LineChartComponent {
         return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
     }
 
-    public randomize(): void {
-        for (let i = 0; i < this.lineChartData.datasets.length; i++) {
-            for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
-                this.lineChartData.datasets[i].data[j] = LineChartComponent.generateNumber(i);
-            }
-        }
-        this.chart?.update();
-    }
-
-    // events
-    public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-        console.log(event, active);
-    }
-
-    public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-        console.log(event, active);
-    }
-
-    public hideOne(): void {
-        const isHidden = this.chart?.isDatasetHidden(1);
-        this.chart?.hideDataset(1, !isHidden);
-    }
-
-    public pushOne(): void {
-        this.lineChartData.datasets.forEach((x, i) => {
-            const num = LineChartComponent.generateNumber(i);
-            x.data.push(num);
-        });
-        this.lineChartData?.labels?.push(`Label ${ this.lineChartData.labels.length }`);
-
-        this.chart?.update();
-    }
-
-    public changeColor(): void {
-        this.lineChartData.datasets[2].borderColor = 'green';
-        this.lineChartData.datasets[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-
-        this.chart?.update();
-    }
-
-    public changeLabel(): void {
-        const tmp = this.newLabel;
-        this.newLabel = this.lineChartData.datasets[2].label;
-        this.lineChartData.datasets[2].label = tmp;
-
-        this.chart?.update();
-    }
 }
